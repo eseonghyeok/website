f62551bd970f655de7dea78aba957bdbe9a42cd4
이성혁 <lsh4270mom@naver.com>
2022-03-18 16:20:39 +0900

    유저 모델 구현

--- app/src/routes/home/home.ctrl.js
+++ app/src/routes/home/home.ctrl.js
@@ -1,6 +1,6 @@
 "use strict";
 
-const UserStorage = require("../../models/UserStorage");
+const User = require("../../models/User");
 
 const output = {
     home: (req, res) => {
@@ -14,24 +14,9 @@ const output = {
 
 const process = {
     login: (req, res) => {
-       const id=req.body.id;
-       const password=req.body.password;
-      
-       const users = UserStorage.getUsers("id", "password");
-       
-       if(users.id.includes(id)){ //DB에 받아온 id가 있으면
-           const idx = users.id.indexOf(id);
-           if(users.password[idx]===password){
-               return res.json({
-                   success: true,
-               });
-           }
-       }
-
-       return res.json({
-           success: false,
-           msg: "로그인에 실패하셨습니다.",
-       });
+        const user = new User(req.body);
+        const response = user.login();
+        return res.json(response);
     },
 };
 
