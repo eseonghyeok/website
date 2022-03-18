0ac5b30bc7465ac62dbb342f14b436a2a701d760
이성혁 <lsh4270mom@naver.com>
2022-03-18 14:40:22 +0900

    백엔드에서 로그인 기능 구현

--- app/src/public/js/home/login.js
+++ app/src/public/js/home/login.js
@@ -13,11 +13,14 @@ function login(){
     };
     
    
-    fetch("/login",{
-        mothod: "POST",
+    fetch("/login", {
+        method: "POST",
         headers: {
             "Content-Type" : "application/json",
         },
         body: JSON.stringify(req),
-    });
+    })
+    .then((res) => res.json())
+    .then((res) => console.log(res));
+
 }
--- app/src/routes/home/home.ctrl.js
+++ app/src/routes/home/home.ctrl.js
@@ -1,14 +1,42 @@
 "use strict";
 
-const home = (req, res) => {
-    res.render("home/index");
+const users={
+    id: ["lsh7569", "lsh75690", "asdf"],
+    password: ["1234", "1234", "123123"],
 };
 
-const login = (req, res) => {
-    res.render("home/login");
+const output = {
+    home: (req, res) => {
+        res.render("home/index");
+    },
+
+    login: (req, res) => {
+        res.render("home/login");
+    },
+};
+
+const process = {
+    login: (req, res) => {
+       const id=req.body.id,
+        password=req.body.password;
+
+       if(users.id.includes(id)){ //DB에 받아온 id가 있으면
+           const idx = users.id.indexOf(id);
+           if(users.password[idx]===password){
+               return res.json({
+                   success: true,
+               });
+           }
+       }
+
+       return res.json({
+           success: false,
+           msg: "로그인에 실패하셨습니다.",
+       });
+    },
 };
 
 module.exports = {
-    home,
-    login,
+    output,
+    process,
 };
