883cfbd6e4d7fbac1b6b246a3dbdb43e1f414abb
이성혁 <lsh4270mom@naver.com>
2022-03-19 17:31:00 +0900

    회원가입 라우팅 구현

--- app/src/public/js/home/register.js
+++ app/src/public/js/home/register.js
@@ -4,16 +4,20 @@ const id = document.querySelector("#id");
 const name = document.querySelector("#name")
 const password = document.querySelector("#psword");
 const confirmPassword = document.querySelector("#confirm-psword");
-const loginBt = document.querySelector("#button");
+const registerBt = document.querySelector("#button");
 
-loginBt.addEventListener("click", register);
+registerBt.addEventListener("click", register);
 
 function register(){
+    if(!id.value)   return alert("아이디를 입력하시오.");
+    if(password.value !== confirmPassword.value){
+        return alert("비밀번호가 일치하지 않습니다.");
+    }
+    
     const req = {
        id: id.value,
        name: name.value,
        password: password.value,
-       confirmPassword: confirmPassword.value,
     };
     
    
